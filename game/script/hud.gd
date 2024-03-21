a7f59718ffebb3db6eac2b9fe7a949e5c3c96089
sugarvoid <gamedev@kitargo.com>
2024-03-21 09:20:57 -0500

    Start on code to reset game.

--- game/script/hud.gd
+++ game/script/hud.gd
@@ -3,9 +3,15 @@ extends CanvasLayer
 @onready var lbl_cards_left = $LblCardsLeft
 @onready var lbl_pairs_left = $LblPairsLeft
 
+var is_game_over: bool = false
 
 func _ready():
 	$GameOverOverlay.hide()
+	$LblGameOver2.hide()
+
+func _unhandled_input(event):
+	if event.is_action_pressed("ui_accept") and is_game_over:
+		print_debug("Reset Game")
 
 func update_cards_left(num:int) -> void:
 	lbl_cards_left.text = str("Cards Left: ", num)
@@ -25,3 +31,9 @@ func update_card_labels(cards: Array[Card]) -> void:
 func show_game_over() -> void:
 	$GameOverOverlay.show()
 	$AnimationPlayer.play("drop_gameover")
+	is_game_over = true
+
+
+func _on_animation_player_animation_finished(anim_name):
+	if anim_name == "drop_gameover":
+		$LblGameOver2.show()
